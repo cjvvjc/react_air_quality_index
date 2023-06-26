4a830b7800c467b303d8d4b46fa099e80f615e6e
Chad Volk <cjvolk@gmail.com>
2023-06-26 16:28:36 -0600

    added CitySearch functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import React, {useState} from 'react'
 import './App.css';
+import CitySearch from './CitySearch';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [airQualityData, setAirQualityData] = useState(null)
+  const [error, setError] = useState(null)
+
+  const getAirQuality = async (city) => {
+    try {
+      const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`)
+  
+      const data = await response.json()
+      console.log(data)
+      if(response.ok && data.status === 'ok') {
+        setAirQualityData(data.data)
+        setError(null)
+      } else {
+        setError("Sorry, we couldn't find the city you were looking for. Try another location nearby or ensure your spelling is correct.")
+        setAirQualityData(null)
+      }
+    } catch (error) {
+      console.error("network error: ", error)
+      setError('Sorry, something went wrong.')
+      setAirQualityData(null)
+    }
+  }
+
   return (
-    <h1>HI</h1>
+    <div>
+      <h1>Air Quality Index Checker</h1>
+      <CitySearch getAirQuality={getAirQuality}/>  
+    </div>
   );
 }
 
